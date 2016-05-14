9a9a1683be1f42f0e7c62649361928e506e08881
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-05-14 08:17:15 +0800

    Demonstrating a Toast

--- app/src/main/java/com/training/android/clickcounter/MainActivity.java
+++ app/src/main/java/com/training/android/clickcounter/MainActivity.java
@@ -1,13 +1,24 @@
 package com.training.android.clickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvCounter = (TextView) findViewById(R.id.tvCounter);
+    }
+
+    public void clickMe(View view) {
+        Toast.makeText(MainActivity.this, "You hit me!", Toast.LENGTH_SHORT).show();
     }
 }
