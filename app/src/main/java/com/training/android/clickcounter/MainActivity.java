7d09446a8767913e493ede798e2e58134b537c82
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-05-14 08:19:46 +0800

    1st way - using onClick xml attribute.

--- app/src/main/java/com/training/android/clickcounter/MainActivity.java
+++ app/src/main/java/com/training/android/clickcounter/MainActivity.java
@@ -4,9 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCounter;
 
     private TextView mTvCounter;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickMe(View view) {
-        Toast.makeText(MainActivity.this, "You hit me!", Toast.LENGTH_SHORT).show();
+        mCounter++;
+        mTvCounter.setText(String.valueOf(mCounter));
     }
 }
