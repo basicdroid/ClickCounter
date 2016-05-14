5798dffa0a4ae169ced6bc200924a38ca29a3997
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-05-14 08:24:53 +0800

    2nd way - using annonymous class

--- app/src/main/java/com/training/android/clickcounter/MainActivity.java
+++ app/src/main/java/com/training/android/clickcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.training.android.clickcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private int mCounter;
 
     private TextView mTvCounter;
+    private Button   mBtnClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTvCounter = (TextView) findViewById(R.id.tvCounter);
+        mBtnClickMe = (Button) findViewById(R.id.btnClickMe);
+
+        // Set the appropriate listener
+        mBtnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickMe();
+            }
+        });
     }
 
-    public void clickMe(View view) {
+    public void clickMe() {
         mCounter++;
         mTvCounter.setText(String.valueOf(mCounter));
     }
